10cc99829bebdbdd3d1df8d5e600d3891054d7d9
Udyan Saha Upal <udyan.upal@gmail.com>
2024-05-09 01:15:02 +0600

    output dir added

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './server.js', // Entry point of your application
   output: {
-    path: path.resolve(__dirname, 'dist'), // Output directory
+    path: path.resolve(__dirname, 'public'), // Output directory
     filename: 'bundle.js' // Output bundle file
   },
   target: 'node', // This tells webpack to compile for usage in a Node.js-like environment
